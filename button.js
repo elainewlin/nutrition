d5b87859808c87c9c3543b97615ef08ce909f771
Elaine Lin <elin1@mit.edu>
2016-05-04 23:12:04 -0400

    Adding files

---
+++ button.js
@@ -0,0 +1,143 @@
+
+//Adds a button to the TinyMCE toolbar with the exhibit logo
+(function($) { 
+	//pretty sure that this line adds your function to tinymce's plugin list
+    $("table").click(function() {
+        console.log("Hello world!");
+    })
+    tinymce.PluginManager.add('exhibit_button', function( editor, url ) {
+	//and this line adds the actual button
+	editor.addButton( 'exhibit_button', { 
+		title: 'Add nutritional information', 
+		image: url + '/foodparsed-pear.png',
+		onclick: function() {
+			editor.insertContent(`
+            <table id="nutritionfacts" class="mceNonEditable">
+                <tbody><tr>
+                    <td align="center" class="header">Nutrition Facts</td>
+                </tr>
+                <tr>
+                    <td bgcolor="#000000"></td>
+                </tr>
+                <tr>
+                    <td>Amount Per Serving</td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Calories</b>
+                            <span id="calories">230</span>
+                        </div>
+                        <div class="dv">Calories from Fat 
+                            <span id="fat-calories">56</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td><div class="dv">% Daily Value</div></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Total Fat</b> 
+                            <span id="fat-grams">6.2g</span>
+                        </div>
+                        <div class="dv">
+                            <span id="fat-dv">10%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="indent">
+                        <div class="labellight">Saturated Fat 
+                            <span id="saturated-grams">3.5g</span>
+                        </div>
+                        <div class="dv">
+                            <span id="saturated-dv">17%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="indent">
+                        <div class="labellight"><i>Trans</i> Fat 
+                            <span id="trans-grams">0.0g</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Cholesterol</b> 
+                            <span id="cholesterol-grams">22mg</span>
+                        </div>
+                        <div class="dv">
+                            <span id="cholesterol-dv">7%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Sodium</b> 
+                            <span id="sodium-grams">618mg</span>
+                        </div>
+                        <div class="dv">
+                            <span id="sodium-dv">26%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Total Carbohydrates</b> 
+                            <span id="carb-grams">32.2g</span>
+                        </div>
+                        <div class="dv">
+                            <span id="carb-dv">11%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="indent">
+                        <div class="labellight">Dietary Fiber 
+                            <span id="fiber-grams">5.2g</span>
+                        </div>
+                        <div class="dv">
+                            <span id="fiber-dv">21%</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="indent">
+                        <div class="labellight">Sugars 
+                            <span id="sugar-grams">3.3g</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label"><b>Protein</b> 
+                            <span id="protein-grams">11.4g</span>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#000000"></td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="label">Vitamin A 
+                            <span id="a-dv">10%</span>
+                        </div>
+                        <div class="dv">Vitamin C 19%</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                      <div class="label">Calcium 22%</div>
+                      <div class="dv">Iron 13%</div>
+                    </td>
+                </tr>      
+            </tbody></table>
+            `);
+		}
+	  }); 
+	}); 
+
+
+})(jQuery);
---
+++ button_wordpress_initialization.php
@@ -0,0 +1,65 @@
+<?php 
+/*
+Plugin Name: Custom Quick Styles
+Plugin URI: http://www.speckygeek.com
+Description: Add custom styles in your posts and pages content using TinyMCE WYSIWYG editor. The plugin adds a Styles dropdown menu in the visual post editor.
+Based on TinyMCE Kit plug-in for WordPress
+http://plugins.svn.wordpress.org/tinymce-advanced/branches/tinymce-kit/tinymce-kit.php
+*/
+/**
+ * Apply styles to the visual editor
+ */ 
+
+add_filter('mce_css', 'plugin_mce_css');
+function plugin_mce_css($url) {
+ 
+    if ( !empty($url) )
+        $url .= ',';
+ 
+    // Retrieves the plugin directory URL
+    // Change the path here if using different directories
+    $url .= plugin_dir_url(__FILE__) . 'editor-styles.css';
+ 
+    return $url;
+}
+ 
+// add_action( 'wp_ajax_plugin_slug_insert_dialog', 'plugin_slug_insert_gistpen_dialog' );
+
+// function plugin_slug_insert_gistpen_dialog() {
+//     $url = plugin_dir_url(__FILE__) . 'form.php'
+//     die(include $url);
+// }
+/* Learn TinyMCE style format options at http://www.tinymce.com/wiki.php/Configuration:formats */
+ 
+/*
+ * Add custom stylesheet to the website front-end with hook 'wp_enqueue_scripts'
+ */
+add_action('wp_enqueue_scripts', 'plugin_post_css');
+ 
+/*
+ * Enqueue stylesheet, if it exists.
+ */
+function plugin_post_css() {
+    $styleUrl = plugin_dir_url(__FILE__) . 'editor-styles.css'; // Customstyle.css is relative to the current file
+    wp_enqueue_style( 'myCustomStyles', $styleUrl );
+}
+
+add_action('admin_head', 'datapress_buttons'); 
+
+function datapress_buttons() {
+	add_filter('mce_external_plugins', 'add_plugins');
+	add_filter('mce_buttons', 'show_datapress_button');
+}
+
+function add_plugins($plugin_array){
+	$plugin_array['exhibit_button'] = plugins_url('/button.js', __FILE__); //exhibit_button is the plugin we're going to create
+	$plugin_array['add_file'] = plugins_url('/button.js', __FILE__);
+	return $plugin_array;
+} //we need to store the js for the button in a file with the name datapress_button_code in the same file as this one
+
+function show_datapress_button($buttons){ //$buttons is an array of buttons that is passed when mce_buttons is called
+	array_push($buttons, 'exhibit_button'); //exhibit_button is the id of the button we're going to create
+	return $buttons;
+}
+
+?>
